4cfb04f55094ce596f82c187e3e489efd2917f8b
José Roberto Azevedo <66346043+joseazevedo47@users.noreply.github.com>
2020-08-06 20:01:41 -0300

    Página de cadastro de categoria feita

---
+++ src/components/FormField/index.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import styled, {css} from 'styled-components';
+
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea {
+    min-height: 150px;
+  }
+
+  input[type="color"]{
+    padding-left: 56px;
+  }
+`;
+
+const Label = styled.label``;
+Label.Text = styled.span`
+  color: #E5E5E5;
+  height: 57px;
+  position: absolute;
+  top: 0;
+  left: 16px;
+
+  display: flex;
+  display: flex;
+  align-items: center;
+
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight:300;
+
+  transition: .1s ease-in-out;
+  
+`;
+
+const Input = styled.input`
+  background: #53585D;
+  color: #F5F5F5;
+  display: block;
+  width: 100%;
+  height: 57px;
+
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585D;
+
+  padding: 16px;
+  margin-bottom: 45px;
+
+  resize: none;
+  border-radius: 4px;
+  transition: border-color .3s;
+
+  }
+  &:focus {
+    border-bottom-color: var(--primary);
+  }
+  
+
+  ${
+    function ({ hasValue }) {
+      console.log(hasValue);
+      return hasValue && css`
+        &:not([type="color"]) + span {
+          transform: scale(.6) translateY(-10px);
+        }
+      `;
+    }
+  }
+
+`;
+
+
+function FormField({
+  label, type, name, value, onChange, as
+}) {
+  const hasValue = Boolean(value.length);
+  const fieldId = `id_${name}`;
+  return (
+    <FormFieldWrapper>
+      <Label htmlFor={fieldId}>
+        <Input
+          as={as}
+          id={fieldId}
+          type={type}
+          name={name}
+          value={value}
+          hasValue={hasValue}
+          onChange={onChange}
+        />
+        <Label.Text>
+          {label}
+        </Label.Text>
+      </Label>
+    </FormFieldWrapper>
+  );
+}
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  as: 'input',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  label: propTypes.string.isRequired,
+  type: propTypes.string,
+  as: propTypes.string,
+  name: propTypes.string.isRequired,
+  value: propTypes.string,
+  onChange: propTypes.func,
+};
+
+export default FormField;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,23 +1,62 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-    return (
-        <>
-            <PageDefault>
-                <h1> Cadastro de Categoria</h1>
-                <form>
-                    <label> Nome da Categoria:
-                        <input type="text"/>
-                        <button>Cadastrar</button>                        
-                    </label>
-                </form>
-                <Link to="/">
-                    Ir para Home
-                </Link>
-            </PageDefault>
-        </>
-    );
+  const initialValues = {
+    name: '',
+    description: '',
+    color: '#000000',
+  };
+  const [categoryValues, setCategoryValues] = useState(initialValues);
+  const [categoryList, setNewCategory] = useState([]);
+
+  function handleChange(eventInfo) {
+    setCategoryValues({
+      ...categoryValues,
+      [eventInfo.target.getAttribute('name')]: eventInfo.target.value,
+    });
+  }
+
+  return (
+    <>
+      <PageDefault>
+        <h1>
+          {' '}
+          Cadastro de Categoria:
+          {categoryValues.name}
+        </h1>
+
+        <form onSubmit={function handleSubmit(eventInfo) {
+          eventInfo.preventDefault();
+          setNewCategory([...categoryList, categoryValues]);
+          console.log(categoryList);
+          setCategoryValues(initialValues);
+        }}
+        >
+          <FormField type="text" name="name" label="Nome da categoria: " value={categoryValues.name} onChange={handleChange} />
+
+          <FormField type="text" as="textarea" name="description" label="Descrição: " value={categoryValues.description} onChange={handleChange} />
+
+          <FormField type="color" name="color" label="Cor: " value={categoryValues.color} onChange={handleChange} />
+
+          <Button>
+            Cadastrar
+          </Button>
+        </form>
+
+        <ul>
+          {categoryList.map((category, index) => (
+            <li key={`${category.name}${index}`}>{category.name}</li>
+          ))}
+        </ul>
+        <Link to="/">
+          Ir para Home
+        </Link>
+      </PageDefault>
+    </>
+  );
 }
 export default CadastroCategoria;
